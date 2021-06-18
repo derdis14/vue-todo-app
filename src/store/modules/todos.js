bd91b1988d62de89cfa8f1da99017024d761ce6b
derdis14 <78613993+derdis14@users.noreply.github.com>
2021-06-18 16:15:57 +0200

    Vuex store: added actions

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 function initialState() {
   return {
     todos: [
@@ -54,4 +56,68 @@ export default {
       }
     },
   },
+  actions: {
+    async loadState({ commit }) {
+      const newState = await fetchAll();
+      if (!newState) {
+        return;
+      }
+
+      newState.nextId = newState.counter.nextId;
+      delete newState.counter;
+      commit("setState", newState);
+    },
+
+    async update({ commit }, updatedTodo) {
+      try {
+        await axios.get(`api/todos/${updatedTodo.id}`);
+        const res = await axios.put(`api/todos/${updatedTodo.id}`, updatedTodo);
+        commit("update", res.data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
+
+    async add({ commit }, newTodo) {
+      try {
+        const nextId = (await axios.get("api/counter")).data.nextId;
+        newTodo.id = nextId;
+        const counter = { nextId: nextId + 1 };
+
+        const res = await axios.post("api/todos", newTodo);
+        await axios.put("api/counter", counter);
+
+        commit("add", res.data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
+
+    async remove({ commit }, id) {
+      try {
+        await axios.delete(`api/todos/${id}`);
+        commit("remove", id);
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
+  },
 };
+
+async function fetchAll() {
+  try {
+    const todos = axios.get("api/todos");
+    const counter = axios.get("api/counter");
+    const res = await Promise.all([todos, counter]);
+    const resState = {
+      todos: res[0].data,
+      counter: res[1].data,
+    };
+    return resState;
+  } catch (error) {
+    console.error(error.message);
+    if (error.response && error.response.status >= 500) {
+      console.log("TERMINAL COMMAND TO START BACKEND: \nnpm run backend");
+    }
+  }
+}
