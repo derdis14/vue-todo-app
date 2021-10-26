344090340ac8c38f22e46d1073a2f4c2c5ca9231
derdis14 <78613993+derdis14@users.noreply.github.com>
2021-10-26 15:57:20 +0200

    added catch all route and NotFoundComponent

---
+++ src/views/NotFoundComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="containerOutside">
+    <div class="containerInside">
+      <h3>Oops ... this URL doesn't exist</h3>
+      <router-link to="/">Go back</router-link>
+    </div>
+  </div>
+</template>
